8bd7ae609a3335a0a5b7bf75e0259c1f134f06ee
Eliezer Portilho <eliezerportilho@gmail.com>
2022-05-07 09:37:32 -0300

    pegar os filmes da api

---
+++ src/App.jsx
@@ -0,0 +1,21 @@
+import React, {useEffect} from "react";
+import Tmdb from "./Tmdb";
+
+export default () => {
+
+    useEffect(() => {
+        const loadAll = async () => {
+            //Pegando a lista total
+            let list = await Tmdb.getHomeList();
+            console.log(list)
+        }
+
+        loadAll()
+    }, []);
+
+    return(
+        <div>
+            Ola mundo
+        </div>
+    )
+}
