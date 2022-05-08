fa23571af16012d614fa620866b5536c8f9fe6d8
Eliezer Portilho <eliezerportilho@gmail.com>
2022-05-08 08:54:55 -0300

    Listagem dos filmes completa

--- src/App.jsx
+++ src/App.jsx
@@ -1,21 +1,46 @@
-import React, {useEffect} from "react";
+import React, {useEffect, useState} from "react";
 import Tmdb from "./Tmdb";
+import MovieRow from "./components/MovieRow";
+import FeatureMovie from "./components/FeatureMovie";
+import "./App.css"
 
 export default () => {
 
-    useEffect(() => {
+    const [movieList, setMovieList] = useState([])
+    const [featureData, setFeatureData] = useState(null)
+
+    useEffect(function(){
         const loadAll = async () => {
             //Pegando a lista total
             let list = await Tmdb.getHomeList();
-            console.log(list)
+            setMovieList(list);
+
+            //pegar o filme em destaque
+            let originals = list.filter(i => i.slug === "originals");
+            let randomChosen = Math.floor(Math.random() * (originals[0].items.results.length -1));
+            let chosen = originals[0].items.results[randomChosen];
+            let chosenInfo = await Tmdb.getMovieInfo(chosen.id, 'tv');
+
+            console.log(chosenInfo)
+
+            setFeatureData(chosenInfo)
         }
 
         loadAll()
-    }, []);
+    }, [])
 
     return(
-        <div>
-            Ola mundo
+        <div className="page">
+
+            {featureData && 
+                <FeatureMovie item={featureData}/>
+            }
+
+            <section className="lists">
+                {movieList.map((item, key) => (
+                    <MovieRow key={key} title={item.title} items={item.items}/>
+                ))}
+            </section>
         </div>
     )
 }
