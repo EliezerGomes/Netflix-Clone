a7f7bce9172dc46412efa92968d05e6e4a7a7d3d
Eliezer Portilho <eliezerportilho@gmail.com>
2022-05-08 15:18:20 -0300

    netflix-clone completo

--- src/App.jsx
+++ src/App.jsx
@@ -2,12 +2,14 @@ import React, {useEffect, useState} from "react";
 import Tmdb from "./Tmdb";
 import MovieRow from "./components/MovieRow";
 import FeatureMovie from "./components/FeatureMovie";
+import Header from "./components/Header";
 import "./App.css"
 
 export default () => {
 
     const [movieList, setMovieList] = useState([])
     const [featureData, setFeatureData] = useState(null)
+    const [blackHeader, setBlackHeader] = useState(false)
 
     useEffect(function(){
         const loadAll = async () => {
@@ -29,8 +31,27 @@ export default () => {
         loadAll()
     }, [])
 
+    useEffect(() => {
+        const scrollListener = () => {
+            if(window.scrollY > 10){
+                setBlackHeader(true)
+            }else{
+                setBlackHeader(false)
+            }
+        }
+
+        window.addEventListener('scroll', scrollListener)
+
+        return() => {
+            window.removeEventListener('scroll', scrollListener)
+        }
+
+    }, [])
+
     return(
         <div className="page">
+
+            <Header black={blackHeader}/>
 
             {featureData && 
                 <FeatureMovie item={featureData}/>
@@ -41,6 +62,19 @@ export default () => {
                     <MovieRow key={key} title={item.title} items={item.items}/>
                 ))}
             </section>
+
+            <footer>
+                Feito com <span role="img" aria-label="coração">❤️</span> por Eliezer Gomës <br />
+                Direitos de imagem para Netflix <br />
+                Dados pegos no site TheMoviedb.org
+            </footer>
+
+            {movieList.length <= 0 &&
+            <div className="loading">
+                <img src="https://media.filmelier.com/noticias/br/2020/03/Netflix_LoadTime.gif" alt="Carregando" />
+            </div>
+            }
+            
         </div>
     )
 }
